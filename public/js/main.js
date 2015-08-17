220a14a6fac746e498e9d36ba04eecec5f1c0431
infinivis <romain.cerantola@heig-vd.ch>
2015-08-17 02:37:17 +0200

    home ok

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
+    console.log($(document).width());
     
-    
-    
+     $("nav").on("click", "li", function() {
+        // here I want to get the clicked id of the li (e.g. bakkerLink)
+        console.log(this);
+        $("nav").attr("data-context", "level1");
+        $("nav ul").attr("data-context", "level1");
+        $('nav li').not(this).attr("data-style", "small");
+        $(this).attr("data-style", "main");
+        
+//        $(this).attr("data-position", "main");
+//        $(this).attr("data-context", "main");
+$("#welcome").attr("data-context", "level1");
+if(!$("header").length){
+    $("body").prepend("<header>");
+}
+
+    });
     
     
     
